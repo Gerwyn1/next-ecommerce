a51feee2ce750cec42501d4494ef52ffd66fe026
Gerwyn Sim <gerwynsim@hotmail.com>
2024-04-17 05:32:56 +0800

    add product page

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,7 +1,14 @@
 // import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
+import Stripe from "stripe";
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+}
 
 const prisma = new PrismaClient();
 export const authOptions = {
@@ -19,4 +26,23 @@ export const authOptions = {
       // },
     }),
   ],
+  events: {
+    createUser: async ({ user }: { user: User }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+      // create stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // also update prisma user with stripe customer id
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
+    },
+  },
 };
